6678c1fd4dec23bea0b73688128a86f37108142b
LongTangGithub <tanglh@g.cofc.edu>
2024-03-04 16:26:16 -0500

    Third Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
+'use client'
+
 import styles from './page.module.css'
 import ZoomParallax from '../components/ZoomParallax/index';
+import Lenis from '@studio-freight/lenis';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+  }, [])
+
+
   return (
     <main className={styles.main}>
       <ZoomParallax />
--- src/components/ZoomParallax/index.jsx
+++ src/components/ZoomParallax/index.jsx
@@ -3,9 +3,10 @@
 import styles from '../styles.module.scss';
 import desert from '../../../public/images/desert.webp';
 import dog from '../../../public/images/dog.webp';
-import cat from '../../../public/images/cat.webp';
+import lizard from '../../../public/images/lizard.webp';
 import man from '../../../public/images/man.webp';
-import sheep from '../../../public/images/sheep.webp';
+import cat from '../../../public/images/cat.webp';
+import horse from '../../../public/images/horse.webp';
 import Image from 'next/image'
 import { useScroll, useTransform, motion } from 'framer-motion';
 import { useRef } from 'react';
@@ -19,21 +20,56 @@ export default function Index () {
     })
 
     const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+    const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+    const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+    const scale7 = useTransform(scrollYProgress, [0, 1], [1, 7]);
+    const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
+    const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+
+    const pictures = [
+        {
+            src: desert,
+            scale:scale4
+        },
+        {
+            src: lizard,
+            scale:scale5
+        },
+        {
+            src: dog,
+            scale:scale6
+        },
+        {
+            src: horse,
+            scale:scale5
+        },
+        {
+            src: man,
+            scale:scale6
+        },
+        {
+            src: cat,
+            scale:scale8
+        }
+    ];
 
     return (
-        <div className={styles.container}>
+        <div ref={container} className={styles.container}>
             <div className={styles.sticky}>
-                <div className={styles.el}>
-                    <motion.div style={{scale: scale4}} className={styles.imageContainer}>
-                        <Image 
-                            src={desert}
-                            fill
-                            alt='image'
-                            placeholder='blur'
-                        />
-                    </motion.div>
-
-                </div>
+                {
+                    pictures.map(({src, scale}, index) => {
+                       return <motion.div style={{scale}} key={ index } className={styles.el}>
+                            <div  className={styles.imageContainer}>
+                                <Image 
+                                    src={src}
+                                    fill
+                                    alt='image'
+                                    placeholder='blur'
+                                />
+                            </div>
+                        </motion.div>
+                    })
+                }
             </div>
         </div>
     )
